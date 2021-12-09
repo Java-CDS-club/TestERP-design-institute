29a239ba0830c11751535369010d80182f5081c2
OskinDI <denis_oskin@rambler.ru>
2021-12-09 20:32:26 +0300

    Init project

---
+++ src/main/java/ru/oskin_di/TestERPdesigninstitute/TestErpDesignInstituteApplication.java
@@ -0,0 +1,13 @@
+package ru.oskin_di.TestERPdesigninstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestErpDesignInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestErpDesignInstituteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/oskin_di/TestERPdesigninstitute/TestErpDesignInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oskin_di.TestERPdesigninstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestErpDesignInstituteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
